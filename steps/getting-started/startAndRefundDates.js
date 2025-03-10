28b318c17d89515c7164b0a45f3e55d63e521a32
hasandemiroz <64849417+hasandemiroz@users.noreply.github.com>
2025-03-10 14:46:42 +0000

    space removed

---
+++ steps/getting-started/startAndRefundDates.js
@@ -0,0 +1,18 @@
+import { Given, Then, When } from "@cucumber/cucumber";
+import { expect} from "@playwright/test";
+import { startApplicationPage, page } from "../../globalPagesSetup.js";
+import { productInfo } from "../../utilities/qa-data-reader.js";
+
+
+
+Given("user is in the enrollment page", async function () {});
+
+Then("the program start date is displayed", async function () {});
+
+Then("the program refund date is displayed", async function () {});
+
+Then("the program start date for the program is correct", async function () {});
+
+Then(
+  "the program refund date for the program is correct", async function () {}
+);
