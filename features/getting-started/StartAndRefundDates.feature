cf60cc98bdeca7d572cabd23bc0f719fcb5d9d80
hasandemiroz <64849417+hasandemiroz@users.noreply.github.com>
2025-07-01 15:38:32 +0100

    Start and Refund dates future add

--- features/getting-started/StartAndRefundDates.feature
+++ features/getting-started/StartAndRefundDates.feature
@@ -12,10 +12,12 @@ Feature: Program start dates and Refund dates
     Background:
         Given user is in the enrollment page
 
+    @sep11-1
     Scenario: Verify that the program start date and refund date are displayed
         Then the program start date is displayed
         Then the program refund date is displayed
 
+    @sep11-2    
     Scenario: Verify that the program start date and refund date are correct
         Then the program start date for the program is correct
         Then the program refund date for the program is correct
--- steps/getting-started/startAndRefundDates.js
+++ steps/getting-started/startAndRefundDates.js
@@ -5,14 +5,32 @@ import { productInfo } from "../../utilities/qa-data-reader.js";
 
 
 
-Given("user is in the enrollment page", async function () {});
+Given("user is in the enrollment page", async function () {
+  await startApplicationPage.login();
+});
 
-Then("the program start date is displayed", async function () {});
+Then("the program start date is displayed", async function () {
+  await expect(startApplicationPage.programStartDate).toBeVisible();
+});
 
-Then("the program refund date is displayed", async function () {});
+Then("the program refund date is displayed", async function () {
+  await expect(startApplicationPage.refundEndDate).toBeVisible();
+});
 
-Then("the program start date for the program is correct", async function () {});
+Then("the program start date for the program is correct", async function () {
+  const ACTUAL_START_DATE =
+    await startApplicationPage.programStartDate.innerText();
+  const EXPECTED_START_DATE = productInfo.startDate;
+  // console.log(`Actual Start date: ${ACTUAL_START_DATE}`);
+  // console.log(`Expected Start date: ${EXPECTED_START_DATE}`);
+  expect(ACTUAL_START_DATE).toBe(EXPECTED_START_DATE);
+});
 
-Then(
-  "the program refund date for the program is correct", async function () {}
-);
+Then("the program refund date for the program is correct", async function () {
+  const ACTUAL_REFUND_DATE =
+    await startApplicationPage.refundEndDate.innerText();
+  const EXPECTED_REFUND_DATE = productInfo.refundDate;
+  // console.log(`Actual Refund date: ${ACTUAL_REFUND_DATE}`);
+  // console.log(`Expected Refund date: ${EXPECTED_REFUND_DATE}`);
+  expect(ACTUAL_REFUND_DATE).toBe(EXPECTED_REFUND_DATE);
+});
